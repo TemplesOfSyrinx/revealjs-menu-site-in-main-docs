001545532521ec81d8bae1276c3ad876d2949dcd
John Chambers-Malewig <johncm@users.noreply.github.com>
2022-03-02 00:28:59 -0600

    Update index.html to include revealjs-menu library (#4)

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,13 @@
 // Also available as an ES module, see:
 // https://revealjs.com/initialization/
 Reveal.initialize({
+    menu: {
+        themes: true,
+        // Specifies the path to the default theme files.
+        themesPath: 'revealjs/dist/theme/',
+    },
+    // Learn about plugins: https://revealjs.com/plugins/
+    plugins: [
+        RevealMenu
+    ]
 });
